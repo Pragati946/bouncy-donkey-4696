6ad474bec23d94b82be33765bb2707422a38394b
sachin9977 <sc09626@gmail.com>
2022-11-09 22:42:42 +0530

    conficts

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,8 @@
-// import { Heading } from "@chakra-ui/react";
-// import Navbar from './Components/CultFit/CultNavbar/CultNavbar';
-// import CultNavbar from './Components/CultFit/CultNavbar/CultNavbar';
 
 function App() {
   return (
     <div>
-     {/* <Navbar/> */}
+      
     </div>
   );
 }
