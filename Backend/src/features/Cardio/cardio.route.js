d0259ae03e94a2bcf7962ae9e3f19374f7dde449
actuallyakshay <akshay.rajput1197@gmail.com>
2022-11-10 02:28:27 +0530

    Added api

--- Backend/src/features/Cardio/cardio.route.js
+++ Backend/src/features/Cardio/cardio.route.js
@@ -4,9 +4,30 @@ const CardioProduct = require("./cardio.model");
 const app = express.Router();
 
 app.get("", async (req, res) => {
+  const { q, page, limit } = req.query;
   try {
-    let products = await CardioProduct.find();
-    res.send(products);
+    if (q) {
+      let temp = new RegExp(q, "i");
+      let products = await CardioProduct.find({ title: temp })
+        .limit(limit)
+        .skip((page - 1) * limit);
+      res.send(products);
+    } else {
+      let products = await CardioProduct.find()
+        .skip((page - 1) * limit)
+        .limit(limit);
+      res.send(products);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+
+app.get("/:id", async (req, res) => {
+  try {
+    let product = await CardioProduct.findById(req.params.id);
+    res.send(product);
   } catch (error) {
     res.status(400).send(error.message);
   }
--- Backend/src/features/Mens/mens.route.js
+++ Backend/src/features/Mens/mens.route.js
@@ -5,13 +5,34 @@ const Men = require("./mens.model");
 const app = express.Router();
 
 app.get("", async (req, res) => {
+  const { page, limit, q } = req.query;
   try {
-    let products = await Men.find();
-    res.send(products);
+    if (q) {
+      let temp = new RegExp(q, "i");
+      let products = await Men.find({ title: temp })
+        .skip((page - 1) * limit)
+        .limit(limit);
+      res.send(products);
+    } else {
+      let products = await Men.find()
+        .skip((page - 1) * limit)
+        .limit(limit);
+      res.send(products);
+      console.log({ products });
+    }
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
+
+app.get("/:id", async (req, res) => {
+  try {
+    let product = await Men.findById(req.params.id);
+    res.send(product);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 module.exports = app;
