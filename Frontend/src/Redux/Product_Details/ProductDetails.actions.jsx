588f71c376c3f4a7ccbed232f7e89c825845f65f
sudarshan441 <sudarshanpujari812@gmail.com>
2022-11-11 23:29:16 +0530

    added

--- Frontend/src/Redux/Product_Details/ProductDetails.actions.jsx
+++ Frontend/src/Redux/Product_Details/ProductDetails.actions.jsx
@@ -1,10 +1,10 @@
 import axios from "axios"
 import { ProductDetails_Error, ProductDetails_Loading, ProductDetails_Success } from "./ProductDetails.actionTypes";
-export const getProductDetail=(id)=>async(dispatch)=>{
+export const getProductDetail=(id,category)=>async(dispatch)=>{
     dispatch({type:ProductDetails_Loading})
     try{
         
-        let response=await  axios.get(`https://cultfit-backend.herokuapp.com/products/${id}`);
+        let response=await  axios.get(`http://localhost:8080/${category}/${id}`);
         let data=response.data
         dispatch({type:ProductDetails_Success,payload:data})
     }catch(err){
