b7aa60ce9ff76105413be34a30fe2bf9ab081467
sudarshan441 <sudarshanpujari812@gmail.com>
2022-11-12 11:27:24 +0530

    added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,16 +5,16 @@
 // import Home_cult from "./Components/CultFit/HomePage_cultfit/Home_cult";
 // import StoreHome from "./Components/CultStore/CultStoreHome/StoreHome";
 // import CultStoreNavbar from "./Components/CultStore/CultStoreNavbar/CultStoreNavbar";
-import HomePage_cultfit from './Pages/cultFit_Pages/Home_cult'
+// import HomePage_cultfit from './Pages/cultFit_Pages/Home_cult'
 
 
 // import {Store} from "../src/Components/CultStore/Store/Store"
 import CultStoreFooter from "./Components/CultStore/CultStoreFooter/CultStoreFooter";
 import ProductDetails  from "./Pages/CultStore/ProductDetails";
-import Crousel1_cultfit from "./Components/CultFit/Fitness_cultfitPage/Crousel1_cultfit";
-import Footer1_cultfit from "./Components/CultFit/Footer/Footer1_cultfit/Footer1_cultfit";
-import Footer2_cultfit from "./Components/CultFit/Footer/Footer1_cultfit/Footer2_cultfit";
-import Home_cult from "./Components/CultFit/HomePage_cultfit/Home_cult";
+// import Crousel1_cultfit from "./Components/CultFit/Fitness_cultfitPage/Crousel1_cultfit";
+// import Footer1_cultfit from "./Components/CultFit/Footer/Footer1_cultfit/Footer1_cultfit";
+// import Footer2_cultfit from "./Components/CultFit/Footer/Footer1_cultfit/Footer2_cultfit";
+// import Home_cult from "./Components/CultFit/HomePage_cultfit/Home_cult";
 import CultStoreNavbar from "./Components/CultStore/CultStoreNavbar/CultStoreNavbar";
 import AllRoutes from "./AllRoutes/AllRoutes";
 
@@ -32,7 +32,7 @@ function App() {
 
      {/* <Home_cult/> */}
   {/* <Crousel1_cultfit></Crousel1_cultfit> */}
-  <CultStoreNavbar></CultStoreNavbar>
+  {/* <CultStoreNavbar></CultStoreNavbar> */}
 
       {/* <Footer1_cultfit />  
       <Footer2_cultfit />   */}
