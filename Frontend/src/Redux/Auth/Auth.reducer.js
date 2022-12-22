274cfc887af2a0afd23c6c0c47c46c3ffce19372
Pragati <pragatitiwari946@gmail.com>
2022-12-22 17:47:41 +0530

    Login and Logout Is working properly now

--- Frontend/src/Redux/Auth/Auth.reducer.js
+++ Frontend/src/Redux/Auth/Auth.reducer.js
@@ -1,49 +1,46 @@
-
-import { AUTH_FAILURE, AUTH_REQUEST, AUTH_SUCCESS, LOGOUT } from "./Auth.types"
-
-let token =localStorage.getItem("token");
-const initValue={
-    // isAuth:!!token,
-    isAuth:false,
-    token:token || "",
-    loading:false,
-    error:false,
+import { FAIL, LOGOUT, REQ, SUC } from "./Auth.types";
+let token = localStorage.getItem("token");
+const IntV = {
+    isAuth: false,
+    token: token || "",
+    loading: false,
+    error: false,
 }
-export const AuthReducer=(state=initValue,{type,payload})=>{
-    localStorage.setItem("token",JSON.stringify(payload));
-switch(type){
-    case AUTH_REQUEST:{
-        return{
-            ...state,
-            loading:true,
-            error:false,
+export const Auth_Reducer = (state = IntV, { type, payload }) => {
+    localStorage.setItem("token", JSON.stringify(payload));
+    switch (type) {
+        case REQ: {
+            return {
+                ...state,
+                loading: true,
+                error: false,
+            }
         }
-    }
-    case AUTH_SUCCESS:{
-        console.log("inside")
-       
-        return{
-            ...state,
-            isAuth:true,
-            loading:false,
-            error:false,
-            token:payload
+        case SUC: {
+            console.log("inside")
+
+            return {
+                ...state,
+                isAuth: true,
+                loading: false,
+                error: false,
+                token: payload
+            }
         }
-    } 
-    case AUTH_FAILURE:{
-        return{
-            ...state,
-            loading:false,
-            error:true, 
-            errorMessage:payload 
+        case FAIL: {
+            return {
+                ...state,
+                loading: false,
+                error: true,
+                errorMessage: payload
+            }
         }
-    }
-    case LOGOUT:
-        localStorage.removeItem("token");
-        return{
-           isAuth:false,
-           token:null
-        }
+        case LOGOUT:
+            localStorage.removeItem("token");
+            return {
+                isAuth: false,
+                token: null
+            }
         default:
             return state
     }
--- Frontend/src/Redux/Auth/Auth.types.js
+++ Frontend/src/Redux/Auth/Auth.types.js
@@ -1,4 +1,4 @@
-export const AUTH_REQUEST = "auth/login/req";
-export const AUTH_SUCCESS = "auth/login/success";
-export const AUTH_FAILURE = "auth/login/failure";
+export const REQ = "auth/login/req";
+export const SUC = "auth/login/suc";
+export const FAIL = "auth/login/fail";
 export const LOGOUT = "auth/login/logout";
